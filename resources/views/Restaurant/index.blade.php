9519450ef1d43279c10e952eb2c5b5047a2130dd
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-08 09:35:27 +0330

    fixed contact form

--- resources/views/Restaurant/index.blade.php
+++ resources/views/Restaurant/index.blade.php
@@ -403,15 +403,15 @@
                         <div class="member-info-content">
                             <h4>{{ $chef->name }}</h4>
                             @switch($chef->position)
-                                @case(1)
-                                    @php $position = "Master Chef" @endphp
-                                    @break
-                                @case(2)
-                                    @php $position = "Patissier" @endphp
-                                    @break
-                                @case(3)
-                                    @php $position = "Cook" @endphp
-                                    @break
+                            @case(1)
+                            @php $position = "Master Chef" @endphp
+                            @break
+                            @case(2)
+                            @php $position = "Patissier" @endphp
+                            @break
+                            @case(3)
+                            @php $position = "Cook" @endphp
+                            @break
                             @endswitch
                             <span>{{ $position }}</span>
                         </div>
@@ -486,34 +486,33 @@
 
             <div class="col-lg-8 mt-5 mt-lg-0">
 
-                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <x-form action="" method="post">
                     <div class="row">
                         <div class="col-md-6 form-group">
-                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name"
-                                required>
+                            <div class="form-group mt-3">
+                                <x-input type="text" name="name" id="name" placeholder="Name" value="{{ old('name') }}" />
+                            </div>
                         </div>
-                        <div class="col-md-6 form-group mt-3 mt-md-0">
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email"
-                                required>
+                        <div class="col-md-6 form-group">
+                            <div class="form-group mt-3">
+                                <x-input type="email" name="email" id="email" placeholder="Email[at]Example.com" value="{{ old('email') }}" />
+                            </div>
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <div class="form-group mt-3">
+                                <x-input type="text" name="subject" id="subject" placeholder="Subject" value="{{ old('subject') }}" />
+                            </div>
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <div class="form-group mt-3">
+                                <x-textarea class="form-control" name="message" id="message" placeholder="Message" row="10">{{ old('message') }}</x-textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mx-auto my-3">
+                            <x-button type="submit">Send Message</x-button>
                         </div>
                     </div>
-                    <div class="form-group mt-3">
-                        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"
-                            required>
-                    </div>
-                    <div class="form-group mt-3">
-                        <textarea class="form-control" name="message" rows="8" placeholder="Message"
-                            required></textarea>
-                    </div>
-                    <div class="my-3">
-                        <div class="loading">Loading</div>
-                        <div class="error-message"></div>
-                        <div class="sent-message">Your message has been sent. Thank you!</div>
-                    </div>
-                    <div class="text-center">
-                        <button type="submit">Send Message</button>
-                    </div>
-                </form>
+                </x-form>
 
             </div>
 
