5e2f27ecdb6df514f5fcd9d7522c1cb595dfdcf6
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-08 18:17:30 +0330

    added asset file css and js

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="Dashboard/assets/img/favicon.png" rel="icon">
-    <link href="Dashboard/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('/Dashboard/assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('/Dashboard/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
@@ -20,16 +20,16 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="Dashboard/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="Dashboard/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="Dashboard/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="Dashboard/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="Dashboard/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="Dashboard/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="Dashboard/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="Dashboard/assets/css/style.css" rel="stylesheet">
+    <link href="{{ asset('/Dashboard/assets/css/style.css') }}" rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: NiceAdmin
@@ -72,17 +72,17 @@
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="Dashboard/assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="Dashboard/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="Dashboard/assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="Dashboard/assets/vendor/echarts/echarts.min.js"></script>
-    <script src="Dashboard/assets/vendor/quill/quill.min.js"></script>
-    <script src="Dashboard/assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="Dashboard/assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="Dashboard/assets/vendor/php-email-form/validate.js"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/chart.js') }}/chart.umd.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('/Dashboard/assets/vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="Dashboard/assets/js/main.js"></script>
+    <script src="{{ asset('/Dashboard/assets/js/main.js') }}"></script>
 
 </body>
 
