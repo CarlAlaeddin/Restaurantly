8e1c5a52f1413eb45705f37289149602762d7bb0
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-10 20:34:16 +0330

    added edit menu admin view

--- resources/views/Admin/pages/menu/edit.blade.php
+++ resources/views/Admin/pages/menu/edit.blade.php
@@ -17,16 +17,56 @@
 @section('content')
     <div class="col-lg-12 my-2 p-5 border rounded bg-white">
         <x-form method="POST" action="{{ route('menu.update', $menu->id) }}" enctype="multipart/form-data">
-            <div class="col-md-6 row">
-                <x-input
-                    name="name"
-                    id="name"
-                    type="text"
-                    placeholder="Enter your
+            <div class="row">
+                <div class="col-md-6">
+                    <x-input
+                        name="name"
+                        id="name"
+                        type="text"
+                        placeholder="Enter your
                     name" value="{{ $menu->name }}"
-                />
+                    />
+                </div>
+                <div class="col-md-6">
+                    <x-input
+                        name="price"
+                        id="price"
+                        type="text"
+                        placeholder="Price"
+                        value="{{ $menu->price }}"
+                    />
+                </div>
+                <div class="col-md-6 my-2">
+                    <x-select name="status" id="status">
+                        <x-option value="" disabled selected>Select status</x-option>
+                        <x-option value="1">Active</x-option>
+                        <x-option value="0">DeActive</x-option>
+                    </x-select>
+                </div>
+                <div class="col-md-6 my-2">
+                    <x-input
+                        name="image"
+                        id="image"
+                        type="file"
+                        placeholder="Enter your image"
+                        value="{{ $menu->name }}"
+                    />
+                </div>
+                <div class="col-md-6 my-2">
+                    <x-input
+                        name="tag"
+                        id="tag"
+                        type="text"
+                        placeholder="Tag"
+                        value="{{ $menu->tag->tag }}"
+                    />
+                </div>
+                <div class="col-md-12">
+                    <x-button type="submit" class="btn btn-primary">
+                        Submit
+                    </x-button>
+                </div>
             </div>
-
         </x-form>
     </div>
 @endsection
