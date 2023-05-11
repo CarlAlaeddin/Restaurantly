2fc3fa0835cf590d1c54c84caca9aa4f67cdce54
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-11 10:13:19 +0330

    added nav item for menu

--- resources/views/Admin/partials/aside.blade.php
+++ resources/views/Admin/partials/aside.blade.php
@@ -1,74 +1,80 @@
 <!-- ======= Sidebar ======= -->
 <aside id="sidebar" class="sidebar">
 
-  <ul class="sidebar-nav" id="sidebar-nav">
+    <ul class="sidebar-nav" id="sidebar-nav">
 
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="index.html">
-        <i class="bi bi-grid"></i>
-        <span>Dashboard</span>
-      </a>
-    </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
 
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#users-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-people-fill"></i>
-        <span>User Management</span>
-        <i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="users-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="{{ route('user.index') }}">
-            <i class="fs-5 bi bi-eyeglasses"></i>
-            <span>All Users</span>
-          </a>
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#users-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-people-fill"></i>
+                <span>User Management</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="users-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{ route('user.index') }}">
+                        <i class="fs-5 bi bi-eyeglasses"></i>
+                        <span>All Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fs-5 bi bi-person-plus-fill"></i>
+                        <span>New User</span>
+                    </a>
+                </li>
+            </ul>
         </li>
-        <li>
-          <a href="">
-            <i class="fs-5 bi bi-person-plus-fill"></i>
-            <span>New User</span>
-          </a>
+        <!-- End Components Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#contact-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-person-rolodex"></i>
+                <span>Contact</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="contact-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{ route('contact.index') }}">
+                        <i class="fs-5 bi bi-eyeglasses"></i>
+                        <span>All Contact</span>
+                    </a>
+                </li>
+            </ul>
         </li>
-      </ul>
-    </li>
-    <!-- End Components Nav -->
+        <!-- End Components Nav -->
 
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#contact-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-person-rolodex"></i>
-        <span>Contact</span>
-        <i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="contact-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="{{ route('contact.index') }}">
-            <i class="fs-5 bi bi-eyeglasses"></i>
-            <span>All Contact</span>
-          </a>
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#menu-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-up"></i>
+                <span>Menu</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="menu-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{ route('menu.create') }}">
+                        <i class="fs-5 bi bi-plus"></i>
+                        <span>New Menu</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('menu.index') }}">
+                        <i class="fs-5 bi bi-eyeglasses"></i>
+                        <span>All Menu</span>
+                    </a>
+                </li>
+            </ul>
         </li>
-      </ul>
-    </li>
-    <!-- End Components Nav -->
+        <!-- End Components Nav -->
 
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#menu-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-menu-up"></i>
-        <span>Menu</span>
-        <i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="menu-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="{{ route('menu.index') }}">
-            <i class="fs-5 bi bi-eyeglasses"></i>
-            <span>All Menu</span>
-          </a>
-        </li>
-      </ul>
-    </li>
-    <!-- End Components Nav -->
+        {{-- <li class="nav-heading">Pages</li> --}}
 
-    {{-- <li class="nav-heading">Pages</li> --}}
-
-  </ul>
+    </ul>
 
 </aside><!-- End Sidebar-->
