5efda26c2286845225f83f430554f384932d98d0
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-11 20:14:49 +0330

    fixed aside tag item

--- resources/views/Admin/partials/aside.blade.php
+++ resources/views/Admin/partials/aside.blade.php
@@ -73,6 +73,28 @@
         </li>
         <!-- End Components Nav -->
 
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#tag-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-tags"></i>
+                <span>Tags</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="tag-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{ route('tag.create') }}">
+                        <i class="fs-5 bi bi-plus"></i>
+                        <span>New Tag</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('tag.index') }}">
+                        <i class="fs-5 bi bi-eyeglasses"></i>
+                        <span>All Tags</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
         {{-- <li class="nav-heading">Pages</li> --}}
 
     </ul>
