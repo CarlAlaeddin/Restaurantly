b13de88273ae859233bef34ebb98b9b6c2decdb7
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-13 09:35:22 +0330

    added primary key table and fillable

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,10 +2,58 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * @var string
+     */
+    protected $table = 'events';
+    /**
+     * @var string[]
+     */
+    protected $hidden = [
+        'updated_at'
+    ];
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'image',
+        'title',
+        'slug',
+        'price',
+        'body',
+        'status',
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
