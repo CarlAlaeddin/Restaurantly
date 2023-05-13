0375e6d05aa442a20b32ca3d84af411a7bf13f04
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-13 10:02:24 +0330

    fixed all structure

--- app/Http/Controllers/ChooseController.php
+++ app/Http/Controllers/ChooseController.php
@@ -3,48 +3,70 @@
 namespace App\Http\Controllers;
 
 use App\Models\Choose;
+use App\Models\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ChooseController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
      */
-    public function index()
+    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
-        //
+        $chooses = Choose::query()->orderBy('id','desc')->paginate(10);
+        return view('Admin.pages.choose.index',compact(['chooses']));
     }
 
     /**
      * Show the form for creating a new resource.
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
      */
-    public function create()
+    public function create(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
-        //
+        return view('Admin.pages.choose.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $choose = new Choose([
+            'user_id'       =>      auth()->user()->id,
+            'title'         =>      $request->get('title'),
+            'description'   =>      $request->get('description'),
+            'status'        =>      $request->get('status'),
+        ]);
+
+        $choose->save();
+        return redirect()->route('choose.index')->with('success', 'The new choose was registered correctly');
     }
 
     /**
      * Display the specified resource.
+     * @param Choose $choose
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
      */
-    public function show(Choose $choose)
+    public function show(Choose $choose): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
-        //
+        return view('Admin.pages.choose.show',compact(['choose']));
     }
 
     /**
      * Show the form for editing the specified resource.
+     * @param Choose $choose
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
      */
-    public function edit(Choose $choose)
+    public function edit(Choose $choose): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
-        //
+        return view('Admin.pages.choose.edit',compact(['choose']));
     }
 
     /**
@@ -52,14 +74,20 @@ class ChooseController extends Controller
      */
     public function update(Request $request, Choose $choose)
     {
-        //
+        $choose->user_id = auth()->user()->id;
+        $choose->update($request->all());
+        return redirect()->route('choose.index')->with('success', 'Your choose has been successfully edited');
     }
+
 
     /**
      * Remove the specified resource from storage.
+     * @param Choose $choose
+     * @return RedirectResponse
      */
-    public function destroy(Choose $choose)
+    public function destroy(Choose $choose): RedirectResponse
     {
-        //
+        $choose->delete();
+        return redirect()->route('choose.index')->with('success', 'Your choose has been successfully deleted');
     }
 }
