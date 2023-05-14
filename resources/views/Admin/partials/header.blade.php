63968fcc872bfb39d9a3a7c3c99b88e2ae94b706
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-14 07:52:05 +0330

    added image profile and logout

--- resources/views/Admin/partials/header.blade.php
+++ resources/views/Admin/partials/header.blade.php
@@ -2,8 +2,8 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="Dashboard/assets/img/logo.png" alt="">
+      <a href="/" class="logo d-flex align-items-center">
+        <img src="{{ asset('Dashboard/assets/img/logo.png') }}" alt="">
         <span class="d-none d-lg-block">NiceAdmin</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -169,21 +169,34 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="Dashboard/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+              @if(!empty(auth()->user()->profile->id))
+                <img src="{{ asset('images/profile/'.auth()->user()->profile->image) }}" alt="Profile" class="rounded">
+              @endif
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ auth()->user()->name }}</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+              <h6>{{ auth()->user()->name }}</h6>
+                @switch(auth()->user()->profile->role)
+                    @case(1)
+                        @php $role = "Admin" @endphp
+                    @break
+                    @case(2)
+                        @php $role = "Writer" @endphp
+                    @break
+                    @case(3)
+                        @php $role = "User" @endphp
+                    @break
+                @endswitch
+              <span>{{ $role }}</span>
             </li>
             <li>
               <hr class="dropdown-divider">
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="{{ route('profile.index') }}">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>
@@ -192,31 +205,14 @@
               <hr class="dropdown-divider">
             </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-gear"></i>
-                <span>Account Settings</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                <i class="bi bi-question-circle"></i>
-                <span>Need Help?</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
+                <x-form action="{{ route('logout') }}" method="post">
+                  <x-button type="submit" class="dropdown-item d-flex align-items-center">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>Sign Out</span>
+                  </x-button>
+                </x-form>
             </li>
 
           </ul><!-- End Profile Dropdown Items -->
