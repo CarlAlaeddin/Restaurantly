7474174235e2561d3fc20b8ba84a73443da22175
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-15 10:23:02 +0330

    show nav item where uri like /

--- resources/views/Restaurant/partials/header.blade.php
+++ resources/views/Restaurant/partials/header.blade.php
@@ -20,12 +20,17 @@
 <header id="header" class="fixed-top d-flex align-items-cente">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
 
-        <h1 class="logo me-auto me-lg-0"><a href="index.html">Restaurantly</a></h1>
+        <h1 class="logo me-auto me-lg-0">
+            <a href="{{ route('index') }}">
+                Restaurantly
+            </a>
+        </h1>
         <!-- Uncomment below if you prefer to use an image logo -->
     <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="{{ asset('Restaurant/assets/img/logo.png') }}" alt="" class="img-fluid"></a>-->
 
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
+                @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->uri === '/')
                 <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                 <li><a class="nav-link scrollto" href="#about">About</a></li>
                 <li><a class="nav-link scrollto" href="#menu">Menu</a></li>
@@ -33,6 +38,7 @@
                 <li><a class="nav-link scrollto" href="#events">Events</a></li>
                 <li><a class="nav-link scrollto" href="#chefs">Chefs</a></li>
                 <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
+                @endif
                 <li class="dropdown"><a href="#"><span>Account</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         @guest
@@ -45,11 +51,14 @@
                         @endauth
                     </ul>
                 </li>
-                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->uri === '/')
+                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                @endif
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
-        <a href="#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
-
+        @if(url()->current() === '/')
+            <a href="#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
+        @endif
     </div>
 </header><!-- End Header -->
