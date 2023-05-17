8e369b9848271290c941c499adca62450b95e440
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-17 14:57:51 +0330

    added event stor

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreEventRequest;
 use App\Models\Event;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Notification;
 
 class PanelController
 {
@@ -26,6 +29,30 @@ class PanelController
     public function event(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         return view('Panel.pages.event.create');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function storeEvent(StoreEventRequest $request): RedirectResponse
+    {
+        $image = time() . '-event' . '.' . $request->file('image')->getClientOriginalExtension();
+
+        $event = new Event([
+            'user_id'    =>     auth()->user()->id,
+            'image'      =>     $image,
+            'title'      =>     $request->get('title'),
+            'price'      =>     $request->get('price'),
+            'body'       =>     $request->get('body'),
+            'status'     =>      0
+        ]);
+
+        $event->save();
+        $user = auth()->user();
+        Notification::send($user, new \App\Notifications\Event($event));
+        $request->image->move('images/event', $image);
+        return redirect()->route('panel.index')->with('success', 'Your event has been successfully created');
     }
 
 }
